5e5ada644669ee3787ae0c01d8e63d262950e53b
kadam shreyash <80761308+Shreyash2801@users.noreply.github.com>
2021-05-31 04:02:15 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+ 
+ 
+int main() {
+ 
+ 
+  int age;
+cout<<"Enter Age of a User:";
+  cin >>age;
+  
+if(age>=18)  {
+    cout<<"\nYour eligible to vote";
+   }else{
+   cout<<"\nYour not eligible to vote"; 
+   }
+   return 0;
+}
